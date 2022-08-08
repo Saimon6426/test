1f458a1fe11e7acaf05e8d12d0eeb5b0c94c82a6
Saimon6426 <Saimon_ne_@mail.ru>
2022-08-08 12:35:02 +0300

    Update js_test.js

--- js_test.js
+++ js_test.js
@@ -2,3 +2,15 @@ var s = 'mystring';
 for (var i = 0; i < s.length; i++) {
  console.log(s.charAt(i));
 }
+
+
+var n = 123;
+var s = '123';
+
+if (n == s) {
+ alert('Переменные равны');
+}
+
+if (n === s) {
+ alert('Переменные идентичны');
+}
