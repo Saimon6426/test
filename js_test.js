35c6e4ce6e75f69b435f2eb3d9b5cb9b8fe2f504
Saimon6426 <Saimon_ne_@mail.ru>
2022-08-26 11:09:40 +0300

    Update js_test.js

--- js_test.js
+++ js_test.js
@@ -1,7 +1,22 @@
 var s = 'mystring';
 for (var i = 0; i < s.length; i++) {
- console.log(s.charAt(i;
+ console.log(s.charAt(i));
 }
+
+function advice(tab){
+var foo = document.getElementById("foo").value;
+var bar = document.getElementById("bar").value;
+tab.loader.load({
+    url : contextPath+"/test.do?method=helloWorld",
+    method : "POST",
+    params : {
+        method : "hi",
+        Foo : foo,
+        Bar : bar,
+        scripts : true
+    }
+});
+}  
 
 
 var n = 123;
